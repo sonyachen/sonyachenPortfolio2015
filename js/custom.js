1791d97e20a5399d7653e20a17a0ad8acfd1ec81
sonyachen <sonyachennh@gmail.com>
2015-10-19 19:25:17 -0400

    2015 Website

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+/* Link animation scroll */
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+/* Back to Top visibility */
+$(window).scroll(function(){
+    var offset = $(window).scrollTop();
+    
+    if(offset>1000){
+        $('#back-to-top').fadeIn();
+	}
+	else {
+	   $('#back-to-top').fadeOut();
+    }
+})
+
+/* Skills Bar animation */
+$(document).ready(function(){
+    
+    $.fn.isInViewport = function(containerSelector, partial) {
+	var $element = this;
+	
+        if($element.length) {
+            var el = $element[0];
+            var rect = el.getBoundingClientRect();
+            
+            if(containerSelector && $(containerSelector).length) {
+                var parentRect = $(containerSelector)[0].getBoundingClientRect();
+            } else {
+                var parentRect = {top: 0, left: 0, bottom: $(window).height(), right: $(window).width()};
+            }
+            
+            if(partial) {
+                return (
+                    rect.left >= parentRect.left &&
+                    rect.right <= parentRect.right &&
+                    ( 
+                    (rect.top >= parentRect.top && rect.top <= parentRect.bottom) ||
+                    (rect.bottom >= parentRect.top && rect.bottom <= parentRect.bottom)
+                    )
+                );
+            } else {
+                return (
+                    rect.top >= parentRect.top &&
+                    rect.left >= parentRect.left &&
+                    rect.bottom <= parentRect.bottom &&
+                    rect.right <= parentRect.right
+                );
+            }
+        }
+    }
+    
+    $(window).scroll(function(){
+        if($('.skillbar').isInViewport()){
+             $('.skillbar').each(function(){
+                var barWidth = $(this).find('.skillbar-percent')[0].innerHTML;
+                $(this).find('.skillbar-bar').animate({
+                    width: barWidth
+                },5000);
+            });
+        }
+    })
+});
