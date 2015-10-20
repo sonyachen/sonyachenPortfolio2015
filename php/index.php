001d60a5f6c6f70ec3111bf40e2274ed5dde5574
sonyachen <sonyachennh@gmail.com>
2015-10-20 09:35:18 -0400

    Updated php to use sendgrid

--- php/index.php
+++ php/index.php
@@ -1,21 +1,39 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
-    $to = 'sonyachennh@gmail.com'; 
-    $subject = 'Hello';
-    $human = $_POST['human'];
-			
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
-	    echo '<p>Your message has been sent!</p>';
-	} else { 
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
-	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
+
+$url = 'https://api.sendgrid.com/';
+$user = 'schenTest';
+$pass = 'sctester1';
+
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => 'example3@sendgrid.com',
+    'subject'   => 'testing from curl',
+    'html'      => 'testing body',
+    'text'      => 'testing body',
+    'from'      => 'example@sendgrid.com',
+  );
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+
 ?>
